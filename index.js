374f1b845e773299890d35a787a121494ac6bb7b
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-08-26 16:33:17 +0500

    new react application created

---
+++ index.js
@@ -0,0 +1,41 @@
+// React is a js library containing already huge code libraried used for creative user interfaces.
+// It was developed by facebook.
+// used for a single page application.
+// browser takes HTML and creates DOM. Now in big applications dealing with DOM becomes very difficult.
+// so we use react library.
+// we don't need querying and updating DOM.
+// creates creates small and reusable components in web page.
+// react will take care of creating and updating DOM.
+// it saves a lot of time.
+// JS vs React: Js step by step and react targets the specific part of UI with in the website.
+//  We can make the components of the website like header, side and front component etc.
+// we can use create react app
+// but for learning we will use the vite tool for learning react.
+// FOR CREATING PROJECT:
+// install node js LTS
+// open command screen.
+// npm create vite@latest
+// are you ok with latest version answer with: y
+// give name to project.
+// select react
+// select JS.
+// copy commands from CLI
+// open the project folder i-e (first react app):
+// npm install in terminal
+// npm run dev
+// follow host link
+// now the components of the project which we created:
+// node modules: package containing all libraries
+// public:contains static files which don't change.
+// src: actual source code.
+// components: parts of code
+// style: contains css styles.
+// eslintrc.cjs: shows errors and warnings.
+// gitignore: contains all the things which are not used in project.
+// index.html: contains the html part
+// package.json: all important info about the project like the app name etc.
+// packagelock.json: contains different values of elements.
+// File extensions:
+// .js: contains normal js code. used for general logic
+//  and .jsx combines js with HTML like tags.
+// makes it easier to design UI
